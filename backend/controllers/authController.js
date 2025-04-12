50b8824994edfb301cd52d2da36e95a679be4f57
roshan6022 <roshansingh6.022@gmail.com>
2025-04-12 14:30:21 +0530

    add response in getUserProfile function

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -95,6 +95,8 @@ export const getUserProfile = async (req, res) => {
     if (!user) {
       return res.status(404).json({ message: "User not found!" });
     }
+
+    res.status(200).json(user); // ✅ Success response
   } catch (error) {
     res.status(500).json({ message: "SERVER ERROR", error: error.message });
   }
