0eba29f78333d2d0862a077d4b37c59555d3b92c
roshan6022 <roshansingh6.022@gmail.com>
2025-04-16 21:38:50 +0530

    Create dashboard page layout

--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -1,5 +1,34 @@
 import React from "react";
-
+import { useContext } from "react";
+import { useUserAuth } from "../../hooks/useUserAuth";
+import { UserContext } from "../../context/userContext";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import { useNavigate } from "react-router-dom";
+import { API_PATHS } from "../../utils/apiPaths.js";
+import axiosInstance from "../../utils/axiosInstance.js";
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  useUserAuth();
+
+  const { user } = useContext(UserContext);
+
+  const navigate = useNavigate();
+
+  const [dashboardData, setDashboardData] = useState(null);
+  const [pieChartData, setPieChartData] = useState([]);
+  const [barChartData, setBarChartData] = useState([]);
+
+  const getDashboardData = async () => {
+    try {
+      const response = await axiosInstance.get(
+        API_PATHS.TASKS.GET_DASHBOARD_DATA
+      );
+      if (response.data) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
+
+  return <DashboardLayout activeMenu="Dashboard">Dashboard</DashboardLayout>;
 }
--- frontend/src/pages/User/UserDashboard.jsx
+++ frontend/src/pages/User/UserDashboard.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import useUserAuth from "../../hooks/useUserAuth";
+import { useContext } from "react";
+import { UserContext } from "../../context/userContext";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
 
 export default function UserDashboard() {
-  return <div>UserDashboard</div>;
+  useUserAuth();
+
+  const { user } = useContext(UserContext);
+  return <DashboardLayout>UserDashboard</DashboardLayout>;
 }
