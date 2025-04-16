331b4b873c2ce6f8a763fa868303adb2686eebbe
roshan6022 <roshansingh6.022@gmail.com>
2025-04-16 20:38:01 +0530

    Integrate SignUp API

--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import AuthLayout from "../../components/layouts/AuthLayout";
 import { validateEmail } from "../../utils/helper";
 import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector";
 import Input from "../../components/Inputs/Input";
-import { Link } from "react-router-dom";
+import { API_PATHS } from "../../utils/apiPaths";
+import { Link, useNavigate } from "react-router-dom";
+import { UserContext } from "../../context/userContext";
+import uploadImage from "../../utils/uploadImage.js";
 export default function SignUp() {
   const [profilePic, setProfilePic] = useState(null);
   const [fullName, setFullName] = useState("");
@@ -12,9 +15,13 @@ export default function SignUp() {
   const [adminInviteToken, setAdminInviteToken] = useState("");
   const [error, setError] = useState(null);
 
+  const { updateUser } = useContext(UserContext);
+  const navigate = useNavigate();
   // Handle SignUp Form Submit
   const handleSignUp = async (e) => {
     e.preventDefault();
+
+    const profilePic = "";
 
     if (!fullName) {
       setError("Please enter full name.");
@@ -33,6 +40,41 @@ export default function SignUp() {
     setError("");
 
     //SignUp API Call
+
+    try {
+      // Upload image if present
+      if (profilePic) {
+        const imgUploadsRes = await uploadImage(profilePic);
+        profileImageUrl = imgUploadsRes.imageUrl || "";
+      }
+      const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+        name: fullName,
+        email,
+        password,
+        profileImageUrl,
+        adminInviteToken,
+      });
+
+      const { token, role } = response.data;
+
+      if (token) {
+        localStorage.setItem("token", token);
+
+        updateUser(response.data);
+        // Redirect based on role
+        if (role === "admin") {
+          navigate("/admin/dashboard");
+        } else {
+          navigate("/user/dashboard");
+        }
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong, Please try again.");
+      }
+    }
   };
   return (
     <AuthLayout>
