418e034e6413dc06e3a8bac44fdaac2c6b385a38
roshan6022 <roshansingh6.022@gmail.com>
2025-07-17 21:10:34 +0530

    fix: minor issues

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,5 +1,3 @@
-export const BASE_URL = import.meta.env.VITE_API_URL;
-
 // utils/apiPaths.js
 export const API_PATHS = {
   AUTH: {
--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { BASE_URL } from "./apiPaths.js";
+import { BASE_URL } from "./apiBase.js";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
