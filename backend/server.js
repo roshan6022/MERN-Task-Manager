cd13e526122f2c0d0717a943c261cb4d4b5ac172
roshan6022 <roshansingh6.022@gmail.com>
2025-07-17 20:50:54 +0530

    fix: minor fix

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 // Middleware to handle CORS
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "*",
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
